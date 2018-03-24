0847bb213ccf03e39dfe25ca9b53f9ef28444996
root <vesphy@gmail.com>
2018-03-24 16:21:52 +0100

    feat: eventsystem

---
+++ Assets/Scripts/Variables/FloatVariable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Scripts.Variables
+{
+    [CreateAssetMenu]
+    public class FloatVariable : Variable<float>
+    {
+        
+    }
+    
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(FloatVariable))]
+    public class FloatVariableEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            FloatVariable myScript = (FloatVariable) target;
+            if (GUILayout.Button("Invoke"))
+            {
+                myScript.ValueChangedEvent.Invoke();
+            }
+        }
+    }
+    
+    #endif
+}
---
+++ Assets/Scripts/Variables/Variable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Scripts.Variables
+{
+    public class Variable<T> : ScriptableObject
+    {
+        [SerializeField] private T _value;
+        public UnityEvent ValueChangedEvent;
+
+        public T Value
+        {
+            get
+            {
+                return _value; 
+                
+            }
+            set
+            {
+                _value = value;
+                ValueChangedEvent.Invoke();
+            }
+            
+        }
+
+    }
+}
