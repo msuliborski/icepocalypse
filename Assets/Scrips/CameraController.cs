1a8d6c8e3280bfd3f15800a2170e9fa7f7e626af
Michal Kusmidrowicz <michal.kusmidrowicz@gmail.com>
2018-03-24 13:59:17 +0100

    player: basic movement

---
+++ Assets/Scrips/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Player;
+    private float _differenceX;
+    private float _differenceY;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        _differenceX = gameObject.transform.position.x - Player.transform.position.x;
+        _differenceY = gameObject.transform.position.y - Player.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        gameObject.transform.position = new Vector3(_differenceX + Player.transform.position.x, _differenceY + Player.transform.position.y, gameObject.transform.position.z);
+
+    }
+};
