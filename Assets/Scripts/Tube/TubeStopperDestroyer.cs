aac69f1d20ff7b94261982c8dc84517de257db92
Michal Kusmidrowicz <michal.kusmidrowicz@gmail.com>
2018-03-25 12:12:44 +0200

    refactor: some player changes

---
+++ Assets/Scripts/Tube/TubeStopperDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeStopperDestroyer : MonoBehaviour
+{
+    public void TubeStopperDestroying()
+    {
+        Destroy(gameObject);
+    }
+	
+}
