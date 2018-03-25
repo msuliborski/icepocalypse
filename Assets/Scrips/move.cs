fbebc4f0f2fd22e752f42310e07279113856fc21
GroveW <37732882+GroveW@users.noreply.github.com>
2018-03-25 13:09:02 +0200

    feat: working enemy prototype

--- Assets/Scrips/move.cs
+++ Assets/Scrips/move.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class move : MonoBehaviour {
+
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(new Vector2(3.0f*Time.deltaTime, 0.0f));
+        //transform.Translate(new Vector2(3.0f*Time.deltaTime, 0.0f));
+        //_rb.velocity = new Vector2(3.0f, _rb.velocity.y);
 	}
 }
