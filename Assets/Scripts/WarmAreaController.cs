875d565c168900162cf79cbe2283bb12f2210b2c
th7nder <vesphy@gmail.com>
2018-05-12 14:21:24 +0200

    feat: freezing effect with visualization

---
+++ Assets/Scripts/WarmAreaController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Scripts.Variables;
+
+public class WarmAreaController : MonoBehaviour {
+    public GameEvent WarmAreaEnter;
+    public GameEvent WarmAreaExit;
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+        WarmAreaEnter.Raise();
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+        WarmAreaExit.Raise();
+	}
+
+}
