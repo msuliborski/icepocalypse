4a922acd852218ed212fec82d0ff36d13eec479a
GroveW <qswdw3ed3@gmail.com>
2018-06-28 01:18:12 +0200

    awdwda

---
+++ Assets/Scripts/StartPanelController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Scripts.Variables;
+
+public class StartPanelController : MonoBehaviour {
+
+    public GameEvent GameStartEvent;
+
+    public void OnGameStartButton()
+    {
+        GameStartEvent.Raise();
+    }
+}
