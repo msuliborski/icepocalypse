8698dd76574445579ab4e5625852a955ec9d1b20
Michal Kusmidrowicz <michal.kusmidrowicz@gmail.com>
2018-06-28 05:20:29 +0200

    head

--- Assets/Scripts/Characters/Enemies/EnemyHeadController.cs
+++ Assets/Scripts/Characters/Enemies/EnemyHeadController.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class EnemyHeadController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Rigidbody2D _rb;
+    CircleCollider2D _col;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            _rb.velocity = new Vector3(0f, 0f, 0f);
+            _rb.isKinematic = true;
+            _col.isTrigger = true;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        _rb = GetComponent<Rigidbody2D>();
+        _col = GetComponent<CircleCollider2D>();
 	}
 	
 	// Update is called once per frame
