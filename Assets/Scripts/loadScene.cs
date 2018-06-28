fb5edb7efaffbec05aa319b58b12a43881e1fc95
Michał Suliborski <msuliborski@gmail.com>
2018-06-28 03:48:25 +0200

    start_fix

---
+++ Assets/Scripts/loadScene.cs
@@ -0,0 +1,12 @@
+﻿ using UnityEngine;
+ using UnityEngine.SceneManagement;
+ 
+ public class loadScene : MonoBehaviour {
+ 
+     public void LoadSceneOnClick()
+     {
+         Debug.Log("sacas");
+         Application.LoadLevel("bezsceny");
+     }
+ 
+ }
